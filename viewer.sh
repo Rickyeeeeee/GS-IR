cd2dcccd3c1e820cdb1ed484f6d912fa9b0950da
Rickyeeeeee <rickyhsu2017@gmail.com>
2025-10-02 10:37:02 +0000

    modify readme

--- viewer.sh
+++ viewer.sh
@@ -1,40 +1,24 @@
-# python viewer.py \
-#     --checkpoint /workspace/work/Outputs/TensoIR_Synthetic/gsir/${scene}/chkpnt35000.pth
-    # --hdri /workspace/data/Datasets/TensoIR_Synthtic/Environment_Maps/high_res_envmaps_1k/bridge.hdr \
-    # --checkpoint /workspace/work/Relighting/MipNerf360/stump/chkpnt40000.pth
-    # --checkpoint /workspace/work/Outputs/Synthetic4Relight/gsir/air_baloons/chkpnt35000.pth
-
-# scene=hotdog
-
-# python pbr_viewer.py \
-# -m /workspace/work/Outputs/Synthetic4Relight/gsir/${scene} \
-# -s /workspace/data/Datasets/Synthetic4Relight/Synthetic4Relight/${scene}/ \
-# --checkpoint /workspace/work/Outputs/Synthetic4Relight/gsir/${scene}/chkpnt35000.pth \
-# --hdri /workspace/data/Datasets/TensoIR_Synthtic/Environment_Maps/high_res_envmaps_1k/bridge.hdr \
-# --eval \
-# --gamma
-
-# scene=ficus
 scene=hotdog
 scene=lego
-# scene=armadillo
+scene=armadillo
+scene=ficus
 
 python pbr_viewer.py \
 -m /workspace/work/Outputs/TensoIR_Synthetic/gsir/${scene} \
--s /workspace/data/Datasets/TensoIR_Synthtic/${scene}/ \
+-s /workspace/data/Datasets/TensoIR_Synthetic/${scene}/ \
 --checkpoint /workspace/work/Outputs/TensoIR_Synthetic/gsir/${scene}/chkpnt35000.pth \
---hdri /workspace/data/Datasets/TensoIR_Synthtic/Environment_Maps/high_res_envmaps_1k/bridge.hdr \
+--hdri /workspace/data/Datasets/TensoIR_Synthetic/Environment_Maps/high_res_envmaps_1k/bridge.hdr \
 --eval \
 --tone \
 --gamma
 
-scene=stump
+# scene=stump
 
-python pbr_viewer.py \
--m /workspace/work/Relighting/MipNerf360/${scene} \
--s /workspace/data/Datasets/MipNerf360/${scene}/ \
---checkpoint /workspace/work/Relighting/MipNerf360/${scene}/chkpnt40000.pth \
---hdri /workspace/data/Datasets/TensoIR_Synthtic/Environment_Maps/high_res_envmaps_1k/bridge.hdr \
---eval \
---tone \
---gamma
+# python pbr_viewer.py \
+# -m /workspace/work/Relighting/MipNerf360/${scene} \
+# -s /workspace/data/Datasets/MipNerf360/${scene}/ \
+# --checkpoint /workspace/work/Relighting/MipNerf360/${scene}/chkpnt40000.pth \
+# --hdri /workspace/data/Datasets/TensoIR_Synthtic/Environment_Maps/high_res_envmaps_1k/bridge.hdr \
+# --eval \
+# --tone \
+# --gamma
